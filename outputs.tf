1973d687e661460bcb887495d66af0874b91d101
Dixit M. KAPURIYA <dixitkapuriya192000@gmail.com>
2021-08-12 16:25:41 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
   
 output "instance_id" {
   description = "Instance id of demo instance"
-  value       = aws_instance.demo_instance.id
+  value       = aws_instance.demo_2_instance.id
 }
 
 output "public_ip" {
   description = "Public ip of demo instance"
-  value       = aws_instance.demo_instance.public_ip
+  value       = aws_instance.demo_2_instance.public_ip
 }
 output "private_dns" {
   description = "List of private DNS names assigned to the instances. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.demo_instance.private_dns
+  value       = aws_instance.demo_2_instance.private_dns
 }
